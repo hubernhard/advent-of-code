4113b421c3e97ef3c029225a7a825b0628420cc6
Bernhard Huber <56220032+hubernhard@users.noreply.github.com>
2023-12-06 09:10:29 +0100

    feat: finish Day 5

--- 2023/day_05/day_05.py
+++ 2023/day_05/day_05.py
@@ -1,7 +1,7 @@
 """Day 5: You Give A Seed A Fertilizer"""
 
+import math
 from pathlib import Path
-from typing import NamedTuple
 
 test_content = """
 seeds: 79 14 55 13
@@ -40,20 +40,6 @@ humidity-to-location map:
 """
 
 
-class Range(NamedTuple):
-    start: int
-    end: int
-
-    def __contains__(self, item: int) -> bool:
-        return self.start <= item <= self.end
-
-    def get_index(self, item: int) -> int:
-        return item - self.start
-
-    def __getitem__(self, item: int) -> int:
-        return self.start + item
-
-
 class Puzzle:
     def __init__(self, content: str):
         content = content.strip().split("\n\n")
@@ -64,39 +50,64 @@ class Puzzle:
         mappings = [mapping.split(":\n")[1] for mapping in content[1:]]
         self.mappings = [self._parse_map(mapping) for mapping in mappings]
 
-    def _parse_map(self, mapping_input: str) -> dict[Range, Range]:
+    def _parse_map(self, mapping_input: str) -> dict[range, range]:
         mapping = {}
         for line in mapping_input.strip().split("\n"):
             dest_start, source_start, length = map(int, line.split())
-            dest_range = Range(dest_start, dest_start + length)
-            source_range = Range(source_start, source_start + length)
+            dest_range = range(dest_start, dest_start + length)
+            source_range = range(source_start, source_start + length)
             mapping[source_range] = dest_range
         return mapping
 
-    def _get_location(self, seed: int) -> int:
+    def _get_location(self, seed: int) -> tuple[int, int]:
         item = seed
+        next_change = math.inf
         for mapping in self.mappings:
+            found_in_range = False
+
             for source, dest in mapping.items():
                 if item in source:
-                    item = dest[source.get_index(item)]
+                    next_change = min(next_change, source.stop - item)
+                    item = dest[source.index(item)]
+                    found_in_range = True
                     break
-        return item
+
+            if not found_in_range:
+                closest = math.inf
+                for source in mapping.keys():
+                    if source.start > item:
+                        closest = min(closest, source.start - item)
+                next_change = min(next_change, closest)
+
+        return item, next_change
 
     def solve_part1(self) -> int:
-        locations = [self._get_location(seed) for seed in self.seeds]
+        locations = [self._get_location(seed)[0] for seed in self.seeds]
         return min(locations)
 
     def solve_part2(self) -> int:
-        return 2
+        seed_ranges = [
+            range(a, a + b) for a, b in zip(self.seeds[0::2], self.seeds[1::2])
+        ]
+        locations = []
+
+        for seed_range in seed_ranges:
+            seed = seed_range.start
+            while seed <= seed_range.stop:
+                location, next_change = self._get_location(seed)
+                locations.append(location)
+                seed += next_change
+
+        return min(locations)
 
 
 if __name__ == "__main__":
     test_puzzle = Puzzle(test_content)
     assert test_puzzle.solve_part1() == 35
-    # assert test_puzzle.solve_part2() == 46
+    assert test_puzzle.solve_part2() == 46
 
     file = Path(__file__).parent / "input.txt"
     content = file.read_text()
     puzzle = Puzzle(content)
     print(f"Part 1: {puzzle.solve_part1()}")
-    # print(f"Part 2: {puzzle.solve_part2()}")
+    print(f"Part 2: {puzzle.solve_part2()}")
