566c063d82965da30440d3b0514ed167b0877fab
Bernhard Huber <56220032+hubernhard@users.noreply.github.com>
2023-12-11 17:22:26 +0100

    feat: finish Day 10

--- 2023/day_10/day_10.py
+++ 2023/day_10/day_10.py
@@ -12,6 +12,32 @@ SJ.L7
 LJ...
 """
 
+test_content2 = """
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+"""
+
+test_content3 = """
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+"""
+
 
 class Point(NamedTuple):
     x: int
@@ -34,21 +60,24 @@ DIRECTIONS = {
     "7": (DOWN, LEFT),
     "F": (DOWN, RIGHT),
     "S": (UP, DOWN, LEFT, RIGHT),
+    ".": (),
 }
 
 
 class Puzzle:
     def __init__(self, content: str):
+        content = content.strip().split("\n")
+        self.nx = len(content[0])
+        self.ny = len(content)
         self.points = {}
-        for y, line in enumerate(content.strip().split("\n")):
+        for y, line in enumerate(content):
             for x, type_ in enumerate(line):
                 point = Point(x, y)
-                if type_ != ".":
-                    self.points[point] = type_
+                self.points[point] = type_
                 if type_ == "S":
                     self.start = point
 
-    def solve_part1(self) -> int:
+    def visit_all(self) -> dict[Point, int]:
         queue = deque()
         queue.append((self.start, 0))
         # Store visited points and the smallest number of steps to reach them
@@ -61,23 +90,40 @@ class Puzzle:
                 p1 = p0 + direction
                 if p1 not in visited:
                     type_ = self.points.get(p1)
-                    if type_ is not None:
+                    if type_ is not None and type_ != ".":
                         # Check if we can go back to p0 from p1
                         neighbors = [p1 + d for d in DIRECTIONS[type_]]
                         if p0 in neighbors:
                             queue.append((p1, n))
                             visited[p1] = n
 
+        return visited
+
+    def solve_part1(self) -> int:
+        visited = self.visit_all()
         return max(visited.values())
 
     def solve_part2(self) -> int:
-        return 2
+        visited = self.visit_all()
+        total = 0
+
+        for y in range(self.ny):
+            inside = False
+            for x in range(self.nx - 1):
+                p = Point(x, y)
+                type_ = self.points.get(p)
+                if p in visited and type_ in ("|", "J", "L"):
+                    inside = not inside
+                if inside and p not in visited:
+                    total += 1
+
+        return total
 
 
 if __name__ == "__main__":
-    test_puzzle = Puzzle(test_content)
-    assert test_puzzle.solve_part1() == 8
-    assert test_puzzle.solve_part2() == 2
+    assert Puzzle(test_content).solve_part1() == 8
+    assert Puzzle(test_content2).solve_part2() == 8
+    assert Puzzle(test_content3).solve_part2() == 10
 
     file = Path(__file__).parent / "input.txt"
     content = file.read_text()
